a45e71d427ff8a4b5a01049db79b9f1c0dfd937d
roy <roy63042026@edu.df.senac.br>
2026-09-16 16:40:34 -0300

    banco finalizado

---
+++ banco/src/main/java/com/mycompany/banco/Banco.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.banco;
+
+/**
+ *
+ * @author roy63042026
+ */
+public class Banco {
+
+    public static void main(String[] args) {
+       
+        
+     Conta Contabrb = new Conta ("roy");
+     
+     Contabrb.sacar (10);
+     
+     Contabrb.depositar (100);
+     
+     Contabrb.sacar (10);
+     
+     Contabrb.extrato ();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
---
+++ banco/src/main/java/com/mycompany/banco/Conta.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.banco;
+
+/**
+ *
+ * @author roy63042026
+ */
+public class Conta {
+   private String usuario;
+   private double saldo;       
+  
+   public Conta (String usuario){
+   this.usuario = usuario;
+   this.saldo = 0.00;
+  }
+   
+   
+   
+   public void depositar (double valor){
+        if (valor > 0)   
+       this.saldo = this.saldo + valor;
+       else{
+       System.out.println("o usuario : " + this.usuario + "nao pode depositar um valor negativo");
+       }
+   }     
+   
+   
+   
+   public void sacar (double valor){
+       if (valor > 0 && valor <= this.saldo)   
+       this.saldo = this.saldo - valor;
+       else{
+       System.out.println("erro");
+       }
+   
+   }       
+   
+   
+   
+   
+   public void extrato (){
+       System.out.println("o usuario : " + this.usuario);
+       System.out.println("Saldo: " + this.saldo);
+   }       
+          
+          
+          
+          
+          
+          
+          
+          
+}
